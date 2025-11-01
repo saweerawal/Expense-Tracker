1e9af73c4a09960b09d3bafe4f3b4fdefa014da8
Sawee Rawal <38999843+saweerawal@users.noreply.github.com>
2025-11-01 06:29:33 +0530

    all functionalities

--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import TrackingHistory, CurrentBalance
 
 # Create your views here.
 
+def delete_transaction(request, id):
+    tracking_history = TrackingHistory.objects.filter(id = id)
+
+    if tracking_history.exists():
+        current_balance, _ = CurrentBalance.objects.get_or_create(id = 1)
+        tracking_history = tracking_history[0]
+        
+        current_balance.current_balance = current_balance.current_balance - tracking_history.amount
+
+        current_balance.save()
+
+
+    tracking_history.delete()
+    return redirect('/')
+
 def index(request):
-    return render(request, "index.html")
+    if request.method == "POST":
+        description = request.POST.get('description')
+        amount = request.POST.get('amount')
+
+        current_balance, _ = CurrentBalance.objects.get_or_create(id=1)
+        
+        expense_type = "CREDIT"
+        if float(amount) < 0:
+            expense_type = "DEBIT"
+        tracking_history = TrackingHistory.objects.create(amount=amount, description=description, expense_type=expense_type, current_balance=current_balance)
+        current_balance.current_balance += float(tracking_history.amount)
+        current_balance.save()
+        
+        return redirect('/')
+    
+    current_balance, _ = CurrentBalance.objects.get_or_create(id = 1)
+    income = 0
+    expense = 0
+    
+    for tracking_history in TrackingHistory.objects.all():
+        if tracking_history.expense_type == "CREDIT":
+            income += tracking_history.amount
+        else:
+            expense += tracking_history.amount
+
+    context = {'income' : income,
+                'expense' : expense , 
+                'transactions' : TrackingHistory.objects.all() , 'current_balance' : current_balance}
+
+    return render(request, "index.html", context=context)
